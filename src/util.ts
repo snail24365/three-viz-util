14da4feb099592365d3b61aec321f1780e9c4090
snail24365 <snail24365@gmail.com>
2022-10-28 16:27:57 +0900

    change minor extra

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { Mesh, Object3D } from "three";
+import { Mesh, Object3D, Vector3 } from "three";
 
 export async function sleep(millisecond: number) {
     return new Promise((resolve) => {
@@ -17,4 +17,10 @@ export function dispose(model: Object3D): void {
     model.children.forEach((child) => {
         dispose(child);
     });
+}
+
+export function getNormal(p1:Vector3, p2:Vector3 ,p3:Vector3):Vector3 {
+    const p2p1 = new Vector3().copy(p2).sub(new Vector3().copy(p1));
+    const p3p2 = new Vector3().copy(p3).sub(new Vector3().copy(p2));
+    return new Vector3().crossVectors(p3p2, p2p1).normalize();
 }
